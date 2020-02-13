c5c2f94a8a95b164670fb14be51ef1085a953797
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2020-02-13 14:14:20 +0100

    adds

---
+++ recept-slutuppg-frontend/src/NewRec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './FirstPage.scss';
+
+export default function FirstPage() {
+    return (
+        <div>
+            <div id="Hero">
+                <div id="Logger">
+                    <h1>Welcome!</h1>     
+                    <button id="Login">Log in here</button>
+                </div>
+            </div>
+            <body>
+
+            </body>
+
+        </div>
+
+
+    )
+}

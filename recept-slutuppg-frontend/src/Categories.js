f6b312ef0285a153687567b6ce1bfe8b79c4ab76
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2020-02-13 14:01:25 +0100

    initial

---
+++ recept-slutuppg-frontend/src/Categories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FirstPage.scss';
+
+export default function FirstPage() {
+    return (
+        <div>
+            <div id="Hero">
+                <div id="Logger">
+                    <h1>Welcome!</h1>     
+                    <div id ="category">
+
+                    </div>
+                </div>
+            </div>
+            <body>
+
+            </body>
+
+        </div>
+
+
+    )
+}

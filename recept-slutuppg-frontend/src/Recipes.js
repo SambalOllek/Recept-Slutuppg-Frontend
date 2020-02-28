1ed841acf0f54d4ebfd8e665071fced46990fb41
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2020-02-28 13:20:58 +0100

    visual changes + start of recipe page

--- recept-slutuppg-frontend/src/Recipes.js
+++ recept-slutuppg-frontend/src/Recipes.js
@@ -1,18 +1,80 @@
 import React from 'react';
-import './FirstPage.scss';
+import './Recipes.scss';
+import kebab from "./assets/kebab.jpg";
+import hamburger from "./assets/hamburger.jpg";
+import hotdog from "./assets/hotdog.jpg";
+import wings from "./assets/wings.jpg";
 
 export default function Recipes() {
 
     return (
         <div>
             <div id="Hero">
-                <div id="Logger">
-                    <h1>Recipes</h1>     
-                    <div id ="category">
+                <div id="Rear">
+                    <header>
+                        <h1>Recipes</h1>
                         <button id="new">Add new recipe</button>
+                    </header>
+                    <div class="r_container">
+                        <div class="card">
+                            <div class="r_imgBx" data-text="Kebab">
+                            <img src= {kebab}/>
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h3>Kebab</h3>
+                                    <p>Lorem ipsum dolor sit amet</p>
+                                    <a href="#">View Recipe</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <div class="r_container">
+                        <div class="card">
+                            <div class="r_imgBx" data-text="Wings">
+                            <img src= {wings}/>
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h3>Wings</h3>
+                                    <p>Lorem ipsum dolor sit amet</p>
+                                    <a href="#">View Recipe</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="r_container">
+                        <div class="card">
+                            <div class="r_imgBx" data-text="Hamburger">
+                            <img src= {hamburger}/>
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h3>Hamburger</h3>
+                                    <p>Lorem ipsum dolor sit amet</p>
+                                    <a href="#">View Recipe</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="r_container">
+                        <div class="card">
+                            <div class="r_imgBx" data-text="HotDog">
+                            <img src= {hotdog}/>
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h3>HotDog</h3>
+                                    <p>Lorem ipsum dolor sit amet</p>
+                                    <a href="#">View Recipe</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+
+                 
                 </div>
-            </div>
             <body>
 
             </body>

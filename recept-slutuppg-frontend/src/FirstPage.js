d7ee176c56b832f5b3140f2971940bedea47570e
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2020-02-28 13:44:08 +0100

    m

--- recept-slutuppg-frontend/src/FirstPage.js
+++ recept-slutuppg-frontend/src/FirstPage.js
@@ -7,8 +7,8 @@ export default function FirstPage() {
             <div id="Hero">
                 <div id="Logger">
                     <h1>Welcome!</h1>     
-                    <button id="Login" href="https://github.com/login/oauth/authorize?client_id=c292a6020ce35b214c2e">Log in here</button>
-                    <a href="https://github.com/login/oauth/authorize?client_id=c292a6020ce35b214c2e" id="Login">Log in here</a>
+                    <a id="Login" 
+                    href="https://github.com/login/oauth/authorize?client_id=c292a6020ce35b214c2e">Log in here</a>
 
                 </div>
             </div>
--- recept-slutuppg-frontend/src/index.js
+++ recept-slutuppg-frontend/src/index.js
@@ -8,7 +8,7 @@ import NewRec from './NewRec.js';
 import Categories from './Categories.js';
 import Recipes from './Recipes.js';
 
-ReactDOM.render(<Categories/>, document.getElementById('root'));
+ReactDOM.render(<FirstPage/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
